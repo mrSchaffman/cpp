5c1574bb3690b278aa5ed5b0b061ef5d56ed83a3
Mr.Schaffman <61944369+mrSchaffman@users.noreply.github.com>
2023-07-20 11:51:52 +0200

    singleton update

--- patterns/creational/singleton/sample1.cpp
+++ patterns/creational/singleton/sample1.cpp
@@ -5,6 +5,7 @@
 */
 
 // Singleton as Global Data
+#include<string>
 namespace logic
 {
     namespace manager
@@ -19,6 +20,10 @@ namespace logic
 
             // eventually add CRUD functions here...
             // ...
+            void add(const std::string & entity){
+                // TODO
+                m_database.push(entity);
+            }
         public:
             ~EntityManager()=default;
 
@@ -30,8 +35,22 @@ namespace logic
             EntityManager( EntityManager&&)=delete;
             EntityManager& operator=(const EntityManager&)=delete;
             EntityManager& operator=( EntityManager&&)=delete;
+
+        private:
+            using DummyData = std::vector<std::string>;
+            DummyData m_database{};
         };
         
     } // namespace manager
     
 } // namespace logic
+
+int main(){
+    using namespace std;
+    std::string entity1="dummy name";
+    
+    using namespace logic::manager;
+    EntityManager::getInstance().add(entity1);
+
+    return 0;
+}
