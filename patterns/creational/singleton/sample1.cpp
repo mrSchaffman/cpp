82da3d44d6c7bcc58e02475b7453e170d138a8f1
Mr.Schaffman <61944369+mrSchaffman@users.noreply.github.com>
2023-07-20 11:36:00 +0200

    update Singleton Design patterns

--- patterns/creational/singleton/sample1.cpp
+++ patterns/creational/singleton/sample1.cpp
@@ -1,5 +1,37 @@
 /**
  * This is only for demonstration purpose
  * Use it at your own Risks
- * 
+ * @barth.feudong
 */
+
+// Singleton as Global Data
+namespace logic
+{
+    namespace manager
+    {
+        class EntityManager
+        {
+        public:
+            EntityManager& getInstance(){
+                static EntityManager instance;
+                return instance;
+            }
+
+            // eventually add CRUD functions here...
+            // ...
+        public:
+            ~EntityManager()=default;
+
+        private:
+            EntityManager()=default;
+
+        private:
+            EntityManager(const EntityManager&)=delete;
+            EntityManager( EntityManager&&)=delete;
+            EntityManager& operator=(const EntityManager&)=delete;
+            EntityManager& operator=( EntityManager&&)=delete;
+        };
+        
+    } // namespace manager
+    
+} // namespace logic
