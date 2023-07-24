ee39fdcd1bbdf9b52e2627685030bcb5809f4c58
Barth. Feudong <61944369+mrSchaffman@users.noreply.github.com>
2023-07-24 21:42:07 +0200

    adding the proxy Design pattern

--- patterns/structural/proxy/sample1.cpp
+++ patterns/structural/proxy/sample1.cpp
@@ -1,5 +1,30 @@
 /**
  * This is only for demonstration purpose
  * Use it at your own Risks
- * 
+ * @barth.feudong
 */
+#include <memory>
+
+class BankAccount
+{
+private:
+    double m_soldo{};
+
+public:
+    BankAccount(/* args */) {}
+    ~BankAccount() {}
+    void deposit(int amout);
+};
+
+int main()
+{
+
+    BankAccount *ba = new BankAccount;
+    ba->deposit(123); // same API
+
+    // using make_shared is the illustration of a proxy
+    auto ba2 = std::make_shared<BankAccount>();
+    ba2->deposit(123); // same API
+
+    return 0;
+}
