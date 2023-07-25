5e587c1f76e8735aa11327c9a897123a83aa0ba0
Barth. Feudong <61944369+mrSchaffman@users.noreply.github.com>
2023-07-25 20:34:33 +0200

    adding the memento Design pattern

--- patterns/behavioral/memento/sample1.cpp
+++ patterns/behavioral/memento/sample1.cpp
@@ -1,5 +1,45 @@
 /**
  * This is only for demonstration purpose
  * Use it at your own Risks
- * 
+ * @barth.feudong
 */
+
+#include<vector>
+namespace entity
+{
+    class AccountMemento
+    {
+    private:
+        double m_balance{};
+
+    public:
+        explicit AccountMemento(double balance) : m_balance{balance} {}
+        double getBalance() const { return m_balance; }
+    };
+    class Account
+    {
+    private:
+        double m_balance{};
+        std::vector<AccountMemento> m_changes{};
+
+    public:
+        explicit Account(double balance = 0.0) : m_balance{balance} {}
+        double getBalance() const { return m_balance; }
+        void deposit(double amount){
+            m_balance += amount;
+            AccountMemento memento(m_balance);
+            m_changes.push_back(memento);
+        }
+        void undo(){
+            if(!m_changes.empty()){
+                auto lastState = m_changes.back();
+                m_balance = lastState.getBalance();
+                m_changes.pop_back();
+            }
+        }
+
+        std::vector<AccountMemento> getChanges() const { return m_changes; }
+    };
+  
+    
+} // namespace entity
